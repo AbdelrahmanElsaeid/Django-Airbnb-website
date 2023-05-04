8e46274f40a030435b9238b0193d3492edbd83b5
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-05-04 08:02:36 +0300

    home page

--- settings/views.py
+++ settings/views.py
@@ -2,15 +2,42 @@ from django.shortcuts import render
 from .models import Settings
 from property.models import Property,Place,Category
 from django.db.models.query_utils import Q
+from django.db.models import Count
+from blog.models import Post 
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 
 def Home(request):
-    place = Place.objects.all()
+    place = Place.objects.all().annotate(property_count = Count('property_place'))
     categories = Category.objects.all()
+    resturant_list = Property.objects.filter(Category__name = 'Resturant')[:5]
+    hotel_list = Property.objects.filter(Category__name = 'Hotels')[:4]
+    places_list = Property.objects.filter(Category__name = 'Places')[:4]
+    recent_posts = Post.objects.all()
 
-    return render(request, 'settings/home.html', {'places':place,'category':categories})
+
+    user_count = User.objects.all().count()
+    hotel_count = Property.objects.filter(Category__name = 'Hotels').count()
+    places_count = Property.objects.filter(Category__name = 'Places').count()
+    resturant_count = Property.objects.filter(Category__name = 'Resturant').count()
+
+    return render(request, 'settings/home.html',{
+        'places':place,
+        'category':categories,
+        'resturant_list': resturant_list,
+        'hotel_list':hotel_list,
+        'places_list':places_list,
+        'recent_posts':recent_posts,
+        'user_count':user_count,
+        'hotel_count':hotel_count,
+        'places_count':places_count,
+        'resturant_count':resturant_count,
+
+
+                    })
 
 
 
