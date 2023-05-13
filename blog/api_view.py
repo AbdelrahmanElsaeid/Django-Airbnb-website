74dee814a06581bed83b47342b9566d9282e2243
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-05-14 00:21:39 +0300

    post_detail_api

--- blog/api_view.py
+++ blog/api_view.py
@@ -2,10 +2,19 @@ from .models import Post
 from .serializers import PostSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET'])
 def post_list_api(request):
     all_posts = Post.objects.all()
     data = PostSerializer(all_posts, many=True).data
+    return Response({'data':data})
+
+
+
+@api_view(['GET'])
+def post_detail_api(request , id):
+    post = get_object_or_404(Post , id=id)
+    data = PostSerializer(post).data
     return Response({'data':data})
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import PostList, PostDetail, PostsByCategory, PostsByTags
-from .api_view import post_list_api
+from .api_view import post_list_api , post_detail_api
 
 app_name = 'blog'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('category/<str:slug>' ,  PostsByCategory.as_view(), name ='post_by_category'),
     path('tag/<slug:slug>' ,  PostsByTags.as_view(), name ='post_by_tag'),
     path('api/list' , post_list_api, name='post_list_api'),
+    path('api/list/<int:id>', post_detail_api, name='post_detail_api'),
 ]
